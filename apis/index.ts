3c716ba981ca821191f9212a18b09df85acdc67a
quochuy2405 <huy01682312920@gmail.com>
2023-05-25 02:18:26 +0700

    update

--- apis/index.ts
+++ apis/index.ts
@@ -1,13 +1,19 @@
 import AxiosServices from './AxiosServices'
 
 export const getProvinces = async () => {
-  return await AxiosServices.get<Array<any>>('https://provinces.open-api.vn/api/')
+  return await AxiosServices.get<Array<any>>(
+    'https://api.viamichelin.com/geocoding/1.0/reverse?lat=0&lon=0&locality=country_code:VN&filter=administrative_area'
+  )
 }
 
 export const getDistrict = async (provinceId: string) => {
-  return await AxiosServices.get(`https://provinces.open-api.vn/api/p/${provinceId}?depth=2`)
+  return await AxiosServices.get(
+    `https://api.viamichelin.com/geocoding/1.0/reverse?lat=0&lon=0&locality=country_code:VN&filter=admin_level:2|administrative_area:${provinceId}`
+  )
 }
 
 export const getAwards = async (districtId: string) => {
-  return await AxiosServices.get(`https://provinces.open-api.vn/api/d/${districtId}?depth=2`)
+  return await AxiosServices.get(
+    `https://api.viamichelin.com/geocoding/1.0/reverse?lat=0&lon=0&locality=country_code:VN&filter=admin_level:3|administrative_area:${districtId}`
+  )
 }
