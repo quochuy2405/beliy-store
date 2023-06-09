4ea6843e51bcad22c066ed190299bff3b7099569
quochuy2405 <huy01682312920@gmail.com>
2023-06-09 15:01:52 +0700

    update

--- pages/register/index.tsx
+++ pages/register/index.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { Register } from '@/components/templates'
-import { create } from '@/firebase/base'
+import { create, findAll } from '@/firebase/base'
 import { db } from '@/firebase/config'
 import { DefaultLayout } from '@/layouts/Layouts'
 import { closeLoading, setLoading } from '@/redux/features/slices/loading'
@@ -8,6 +8,7 @@ import { schemaRegister } from '@/resolvers/resgiter'
 import { collection } from '@firebase/firestore'
 import { yupResolver } from '@hookform/resolvers/yup'
 import { useRouter } from 'next/router'
+import { enqueueSnackbar } from 'notistack'
 import { ReactElement } from 'react'
 import { useForm } from 'react-hook-form'
 import { useDispatch } from 'react-redux'
@@ -26,33 +27,39 @@ const RegisterPage = () => {
   const handleSubmit = async (data) => {
     dispatch(setLoading({ status: true, mode: 'default', title: 'Đang tạo tài khoản' }))
     const accountRef = collection(db, 'account_users')
-    await create(accountRef, data)
-      .then((res) => {
-        console.log(res)
-        dispatch(
-          setLoading({
-            status: true,
-            mode: 'success',
-            title: (
-              <div className="flex flex-col pt-3 justify-center items-center">
-                <p>Tạo thành công</p>
-                <button
-                  onClick={() => {
-                    dispatch(closeLoading())
-                    router.push('/')
-                  }}
-                  className="flex-1 mt-2 items-center py-2 px-4 text-xs font-medium text-center text-white bg-emerald-400 rounded-lg focus:ring-4 focus:ring-emerald-200 hover:bg-emerald-600"
-                >
-                  Về trang đăng nhập
-                </button>
-              </div>
+    await findAll(accountRef, [['email', data.email]]).then(async (res) => {
+      if (!res.length) {
+        await create(accountRef, data)
+          .then((res) => {
+            console.log(res)
+            dispatch(
+              setLoading({
+                status: true,
+                mode: 'success',
+                title: (
+                  <div className="flex flex-col pt-3 justify-center items-center">
+                    <p>Tạo thành công</p>
+                    <button
+                      onClick={() => {
+                        dispatch(closeLoading())
+                        router.push('/')
+                      }}
+                      className="flex-1 mt-2 items-center py-2 px-4 text-xs font-medium text-center text-white bg-emerald-400 rounded-lg focus:ring-4 focus:ring-emerald-200 hover:bg-emerald-600"
+                    >
+                      Về trang đăng nhập
+                    </button>
+                  </div>
+                )
+              })
             )
           })
-        )
-      })
-      .catch((error) => {
-        console.log(error)
-      })
+          .catch((error) => {
+            console.log(error)
+          })
+      } else {
+        enqueueSnackbar('Tài khoản đã tồn tại', { variant: 'error' })
+      }
+    })
   }
   const props = {
     methods,
