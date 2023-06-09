1bb14fa9115f5376e3165391e1e58e3a0ac04988
quochuy2405 <huy01682312920@gmail.com>
2023-06-09 14:48:15 +0700

    update

--- components/templates/Login.tsx
+++ components/templates/Login.tsx
@@ -30,7 +30,7 @@ const Login = ({ methods, handleSubmit }) => {
                         {...field}
                         id="email"
                         className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
-                        placeholder="example@gmail.com"
+                        placeholder=""
                         required
                       />
                       <small className="text-red-400 text-xs">
