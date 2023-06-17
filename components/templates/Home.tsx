b9dea84d30ad34499b93a9b68e5d087279f1c0ee
quochuy2405 <huy01682312920@gmail.com>
2023-06-17 11:55:37 +0700

    update

--- components/templates/Home.tsx
+++ components/templates/Home.tsx
@@ -1,8 +1,12 @@
+import { ImageOptimizing } from '../atoms'
 import { ProductList, PromoteSection } from '../organisms'
 
 const Home = () => {
   return (
     <div>
+      <div className="h-[110vh]">
+        <ImageOptimizing src="/banner2.png" objectFit="cover" />
+      </div>
       <PromoteSection />
       <ProductList conditions={[]} title="Sản phẩm nổi bật" />
     </div>
