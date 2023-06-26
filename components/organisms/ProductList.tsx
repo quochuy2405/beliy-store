b384dcf2cd7505f8004d6c3ac9cb982f7746a64e
quochuy2405 <huy01682312920@gmail.com>
2023-06-26 23:46:50 +0700

    update

--- components/organisms/ProductList.tsx
+++ components/organisms/ProductList.tsx
@@ -84,7 +84,7 @@ const ProductList: React.FC<ProductListProps> = ({ id, conditions, title }) => {
   }, [conditions])
   return (
     <section className="bg-white" id={id}>
-      <div className="mx-auto max-w-2xl px-4 py-8 sm:px-6  lg:max-w-7xl lg:px-8">
+      <div className="mx-auto max-w-2xl px-4 py-1 sm:px-6  lg:max-w-7xl lg:px-8">
         <Controller
           name="products"
           control={control}
--- components/templates/Product.tsx
+++ components/templates/Product.tsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import { Controller, UseFormReturn } from 'react-hook-form'
 import { ProductList } from '../organisms'
+import Image from 'next/image'
 
 interface ProductProps {
   gender?: string
@@ -9,32 +10,16 @@ interface ProductProps {
 }
 const Product: React.FC<ProductProps> = ({ gender, stateStore }) => {
   return (
-    <div className="py-6">
-      <div className="fixed w-10 h-fit flex flex-col gap-1 top-[30%] right-[5%]">
-        {/* <Link
-          href="/product#AT"
-          className="relative w-10 h-10 bg-white rounded-full shadow-xl flex items-center justify-center after:content-['→'] after:absolute after:w-10 after:h-10 after:hidden hover:after:block ease-linear after:-left-5 after:top-[10px]"
-          passHref
-          scroll
-        >
-          <FaTshirt size={18} />
-        </Link>
-        <Link
-          href="/product#AK"
-          className="relative w-10 h-10 bg-white rounded-full shadow-xl flex items-center justify-center after:content-['→'] after:absolute after:w-10 after:h-10 after:hidden hover:after:block ease-linear after:-left-5 after:top-[10px]"
-          passHref
-          scroll
-        >
-          <GiSleevelessJacket size={18} />
-        </Link>
-        <Link
-          href="/product#Q"
-          className="relative w-10 h-10 bg-white rounded-full shadow-xl flex items-center justify-center after:content-['→'] after:absolute after:w-10 after:h-10 after:hidden hover:after:block ease-linear after:-left-5 after:top-[10px]"
-          passHref
-          scroll
-        >
-          <GiArmoredPants size={18} />
-        </Link> */}
+    <div className="pb-6">
+      <div className="h-[95vh]">
+        <Image
+          alt=""
+          width={2000}
+          height={2000}
+          src="/bannerpro.png"
+          className="
+        w-full h-full object-cover"
+        />
       </div>
       <Controller
         name="categories"
