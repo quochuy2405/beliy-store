ef80ece8f0e711d774a041bad21f38af702025cd
quochuy2405 <huy01682312920@gmail.com>
2023-08-15 10:28:39 +0700

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,7 +23,7 @@ export default defineNuxtConfig({
             charset: 'utf-8',
             viewport: 'width=device-width, initial-scale=1',
             title: 'Beliy Stresswear 👋🏻',
-            link: [{ rel: 'icon', type: 'image/x-icon', href: '/logo.svg' }],
+
             meta: [
                 {
                     name: 'keywords',
@@ -81,6 +81,11 @@ export default defineNuxtConfig({
             ],
             link: [
                 { rel: 'icon', type: 'image/x-icon', href: '/logo.svg' },
+                {
+                    rel: 'icon',
+                    type: 'image/vnd.microsoft.icon',
+                    href: '/logo.svg',
+                },
                 {
                     rel: 'stylesheet',
                     href: 'https://fonts.googleapis.com/css2?family=Didact+Gothic&family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap',
